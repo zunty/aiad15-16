940cbf9491f25c1eb36e1dda9f44f27024727c72
joaoRibeiro <jjuniorib@hotmail.com>
2015-11-10 14:54:12 +0000

    Warning fixing

--- src/Agents/Person.java
+++ src/Agents/Person.java
@@ -1,69 +1,70 @@
 package Agents;
-import org.joda.time.*;
 
 import PersonClasses.Schedule;
-import PersonClasses.Assignment;
-import PersonClasses.Proposal;
-import jade.core.AID;
-import jade.core.behaviours.*;
 import jade.core.Agent;
-import jade.lang.acl.ACLMessage;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
 
-import java.io.*;
-import java.util.Vector;
 
 
 public class Person extends Agent {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String name;
 	Schedule schedule;
 
 	
 	// método setup -------------------------------------------------------------------------------
 	protected void setup() {
-		
-		Object[] args = getArguments();
-		if(args != null && args.length > 0) {
-			this.name = (String) args[0];
-			System.out.println("{"+Person.this.name+"}Arguments: " + args.toString());
-		} else {
-			System.out.println("{"+Person.this.name+"}I don't have arguments.");
-		}
 
 		// Base para a comunicação de Agentes
 		DFAgentDescription dfd = new DFAgentDescription();
 		dfd.setName(getAID());
 		ServiceDescription sd = new ServiceDescription();
+		this.name = getName();
 		sd.setName(getName());
 		System.out.println("I am " + this.name + " and this is my getName(): " + getName());
 		sd.setType("Person");
 		dfd.addServices(sd);
 
-		try {
+		/*try {
 			DFService.register(this, dfd);
 		} catch(FIPAException e) {
 			e.printStackTrace();
-		}
+		}*/
 
 		Person.this.schedule = new Schedule(Person.this.name);
 
 		/**
 		 * 
-		 * Inserir tarefas e participantes para essas tarefas 
+		 * Inserir tarefas e participantes para essas tarefas para futuros tes
 		 * 
+		 * //System.out.println("{"+Person.this.name+"} has assignments with "+Person.this.schedule.getPeopleIHaveAssignmentsWith().toString());
+		 *
 		 */
 		
-		//System.out.println("{"+Person.this.name+"} has assignments with "+Person.this.schedule.getPeopleIHaveAssignmentsWith().toString());
-
+		
 		waitingForProposals();
 	}
 
 	public void waitingForProposals() {
 		//TODO
+	    try {
+	        while (true) {
+	            System.out.println("Waiting for new proposals...");
+	            Thread.sleep(5 * 1000);
+	        }
+	    } catch (InterruptedException e) {
+	        e.printStackTrace();
+	    }
+		
+		//At the end
+		exit();
 	}
 
 	protected void exit() {
